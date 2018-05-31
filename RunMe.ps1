1985ffb5a864b0afa83365665d66c185ac49dad0
PrzemyslawKlys <pklys@evotec.pl>
2018-05-31 19:31:31 +0200

    Removed from running code without runspaces.

--- RunMe.ps1
+++ RunMe.ps1
@@ -7,7 +7,7 @@ $Computers = 'AD2', 'AD1', 'AD1', 'EVO1', 'AD1', 'EVO1', 'EVO1', 'AD1', 'AD2', '
 $Services = 'WinRM', 'VSS'
 
 # This is to show how long it takes for "standard" approach
-PSGetServiceNoRunspaces -Computers $Computers -Services $Services -Verbose | Format-Table -AutoSize
+#PSGetServiceNoRunspaces -Computers $Computers -Services $Services -Verbose | Format-Table -AutoSize
 
 # This is the REAL speedup
 PSGetService -Computers $Computers -Services $Services -Verbose | Format-Table -AutoSize
