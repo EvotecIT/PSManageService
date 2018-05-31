b6015fbb0ff7410218c75266c1e4cc963f534e44
PrzemyslawKlys <pklys@evotec.pl>
2018-05-31 19:21:29 +0200

    Updates

--- PSManageService.psm1
+++ PSManageService.psm1
@@ -8,7 +8,14 @@ function PSGetService {
         $Services
     )
     if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) { $Verbose = $true } else { $Verbose = $false }
-    Write-Verbose 'Get-Service - Starting....'
+    Write-Verbose 'Get-Service - Starting parallel processing....'
+
+    $ComputersToProcess = ($Computers | Measure-Object).Count
+    $ServicesToProcess = ($Services | Measure-Object).Count
+    Write-Verbose -Message "Get-Service - Computers to process: $ComputersToProcess"
+    Write-Verbose -Message "Get-Service - Computers List: $Computers"
+    Write-Verbose -Message "Get-Service - Services to process: $ServicesToProcess"
+
     $MeasureTotal = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
 
     ### Define Runspace START
@@ -29,29 +36,30 @@ function PSGetService {
                     [string]$ServiceName,
                     [bool] $Verbose
                 )
-                Write-Verbose "Get-Service - [i] Processing $Computer with $ServiceName"
+
                 $Measure = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
                 if ($Verbose) {
                     $verbosepreference = 'continue'
                 }
+                Write-Verbose "Get-Service - [i] Processing $Computer with $ServiceName"
                 $GetService = Get-Service -ComputerName $Computer -Name $ServiceName
                 if ($GetService) {
                     $ServiceStatus = [ordered] @{}
                     $ServiceStatus.Computer = $Computer
                     $ServiceStatus.Status = $GetService.Status
                     $ServiceStatus.Name = $GetService.Name
-                    $ServiceStatus.DisplayName = $GetService.DisplayName
                     $ServiceStatus.ServiceType = $GetService.ServiceType
                     $ServiceStatus.StartType = $GetService.StartType
+                    $ServiceStatus.DisplayName = $GetService.DisplayName
                     $ServiceStatus.TimeProcessing = $Measure.Elapsed
                 } else {
                     $ServiceStatus = [ordered]@{}
                     $ServiceStatus.Computer = $Computer
                     $ServiceStatus.Status = 'N/A'
                     $ServiceStatus.Name = $ServiceName
-                    $ServiceStatus.DisplayName = 'N/A'
                     $ServiceStatus.ServiceType = 'N/A'
                     $ServiceStatus.StartType = 'N/A'
+                    $ServiceStatus.DisplayName = 'N/A'
                     $ServiceStatus.TimeProcessing = $Measure.Elapsed
                 }
                 Write-Verbose "Get-Service - [i] Processed $Computer with $ServiceName - Time elapsed: $($Measure.Elapsed)"
@@ -94,4 +102,54 @@ function PSGetService {
     Write-Verbose "Get-Service - Ending....$($measureTotal.Elapsed)"
     # return Data
     return $AllStatus #| Select-object Computer, Name, DisplayName, Status
+}
+
+
+function PSGetServiceNoRunspaces {
+    [cmdletbinding()]
+    param (
+        $Computers,
+        $Services
+    )
+    if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) { $Verbose = $true } else { $Verbose = $false }
+    $AllStatus = @()
+    Write-Verbose 'Get-Service - Starting standard processing....'
+    $ComputersToProcess = ($Computers | Measure-Object).Count
+    $ServicesToProcess = ($Services | Measure-Object).Count
+    Write-Verbose -Message "Get-Service - Computers to process: $ComputersToProcess"
+    Write-Verbose -Message "Get-Service - Computers List: $Computers"
+    Write-Verbose -Message "Get-Service - Services to process: $ServicesToProcess"
+    $MeasureTotal = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
+
+    $GetService = Get-Service -ComputerName $Computers -Name $Services
+    # $GetService
+    if ($GetService) {
+        foreach ($Service in $GetService) {
+
+            $ServiceStatus = [ordered] @{}
+            $ServiceStatus.Computer = $Service.MachineName
+            $ServiceStatus.Status = $Service.Status
+            $ServiceStatus.Name = $Service.Name
+            $ServiceStatus.ServiceType = $Service.ServiceType
+            $ServiceStatus.StartType = $Service.StartType
+            $ServiceStatus.DisplayName = $Service.DisplayName
+
+            $AllStatus += $ServiceStatus
+        }
+    } else {
+        $ServiceStatus = [ordered]@{}
+        $ServiceStatus.Computer = $Computer
+        $ServiceStatus.Status = 'N/A'
+        $ServiceStatus.Name = $ServiceName
+        $ServiceStatus.ServiceType = 'N/A'
+        $ServiceStatus.StartType = 'N/A'
+        $ServiceStatus.DisplayName = 'N/A'
+        $AllStatus += $ServiceStatus
+    }
+
+
+    $MeasureTotal.Stop()
+    Write-Verbose "Get-Service - Ending....$($measureTotal.Elapsed)"
+
+    return $AllStatus.ForEach( {[PSCustomObject]$_})
 }
--- RunMe.ps1
+++ RunMe.ps1
@@ -1,7 +1,10 @@
 
+Clear-Host
 Import-Module PSManageService -Force
 
-$Computers = 'AD1' #, 'AD2'
+$Computers = 'AD2', 'AD1', 'AD1', 'EVO1', 'AD1', 'EVO1', 'EVO1', 'AD1', 'AD2', 'AD2'
 $Services = 'WinRM', 'VSS'
 
-PSGetService -Computers $Computers -Services $Services -Verbose
+PSGetServiceNoRunspaces -Computers $Computers -Services $Services -Verbose | Format-Table -AutoSize
+
+PSGetService -Computers $Computers -Services $Services -Verbose | Format-Table -AutoSize
