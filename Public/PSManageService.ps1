5b147f9cd5c0b6a97dbadcfc3401e73486c5563b
PrzemyslawKlys <pklys@evotec.pl>
2018-06-20 22:07:26 +0200

    new feature - list services

--- Public/PSManageService.ps1
+++ Public/PSManageService.ps1
@@ -6,38 +6,49 @@ $sbGetService = {
     )
     $Measure = [System.Diagnostics.Stopwatch]::StartNew() # Timer Start
     if ($Verbose) { $verbosepreference = 'continue' }
-    Write-Verbose "Get-Service - [i] Processing $Computer with $ServiceName"
-    $GetService = Get-Service -ComputerName $Computer -Name $ServiceName
-    if ($GetService) {
-        $ServiceStatus = [ordered] @{}
-        $ServiceStatus.Computer = $Computer
-        $ServiceStatus.Status = $GetService.Status
-        $ServiceStatus.Name = $GetService.Name
-        $ServiceStatus.ServiceType = $GetService.ServiceType
-        $ServiceStatus.StartType = $GetService.StartType
-        $ServiceStatus.DisplayName = $GetService.DisplayName
-        $ServiceStatus.TimeProcessing = $Measure.Elapsed
+
+    if ($ServiceName -eq '') {
+        Write-Verbose "Get-Service - [i] Processing $Computer for all services"
+        $GetServices = Get-Service -ComputerName $Computer
     } else {
-        $ServiceStatus = [ordered]@{}
-        $ServiceStatus.Computer = $Computer
-        $ServiceStatus.Status = 'N/A'
-        $ServiceStatus.Name = $ServiceName
-        $ServiceStatus.ServiceType = 'N/A'
-        $ServiceStatus.StartType = 'N/A'
-        $ServiceStatus.DisplayName = 'N/A'
-        $ServiceStatus.TimeProcessing = $Measure.Elapsed
+        Write-Verbose "Get-Service - [i] Processing $Computer with $ServiceName"
+        $GetServices = Get-Service -ComputerName $Computer -Name $ServiceName
+    }
+    $ServiceList = @()
+    foreach ($GetService in $GetServices) {
+
+        if ($GetService) {
+            $ServiceStatus = [ordered] @{}
+            $ServiceStatus.Computer = $Computer
+            $ServiceStatus.Status = $GetService.Status
+            $ServiceStatus.Name = $GetService.Name
+            $ServiceStatus.ServiceType = $GetService.ServiceType
+            $ServiceStatus.StartType = $GetService.StartType
+            $ServiceStatus.DisplayName = $GetService.DisplayName
+            $ServiceStatus.TimeProcessing = $Measure.Elapsed
+        } else {
+            $ServiceStatus = [ordered]@{}
+            $ServiceStatus.Computer = $Computer
+            $ServiceStatus.Status = 'N/A'
+            $ServiceStatus.Name = $ServiceName
+            $ServiceStatus.ServiceType = 'N/A'
+            $ServiceStatus.StartType = 'N/A'
+            $ServiceStatus.DisplayName = 'N/A'
+            $ServiceStatus.TimeProcessing = $Measure.Elapsed
+        }
+        $ServiceList += $ServiceStatus
     }
     Write-Verbose "Get-Service - [i] Processed $Computer with $ServiceName - Time elapsed: $($Measure.Elapsed)"
     $Measure.Stop()
-    return $ServiceStatus.ForEach( {[PSCustomObject]$_})
+    return $ServiceList.ForEach( {[PSCustomObject]$_})
 }
 
 function Get-PSService {
     [cmdletbinding()]
     param (
-        $Computers,
-        $Services,
-        [int] $maxRunspaces = [int]$env:NUMBER_OF_PROCESSORS + 1
+        [string[]] $Computers = $Env:COMPUTERNAME,
+        [string[]] $Services,
+        [int] $MaxRunspaces = [int]$env:NUMBER_OF_PROCESSORS + 1
     )
     if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) { $Verbose = $true } else { $Verbose = $false }
     Write-Verbose 'Get-Service - Starting parallel processing....'
@@ -53,17 +64,28 @@ function Get-PSService {
     $runspaces = @()
     ### Define Runspace END
 
-    foreach ($ServiceName in $Services) {
-        foreach ($Computer in $Computers) {
 
-            # processing runspace start
+    foreach ($Computer in $Computers) {
+        if ($Services -ne $null) {
+            foreach ($ServiceName in $Services) {
+                Write-Verbose "Get-Service - Getting service $ServiceName on $Computer"
+                # processing runspace start
+                $Parameters = @{
+                    Computer    = $Computer
+                    ServiceName = $ServiceName
+                    Verbose     = $Verbose
+                }
+                $runspaces += Start-Runspace -ScriptBlock $sbGetService -Parameters $Parameters -RunspacePool $pool
+                # processing runspace end
+            }
+        } else {
+            Write-Verbose "Get-Service - Getting all services on $Computer"
             $Parameters = @{
                 Computer    = $Computer
-                ServiceName = $ServiceName
+                ServiceName = ''
                 Verbose     = $Verbose
             }
             $runspaces += Start-Runspace -ScriptBlock $sbGetService -Parameters $Parameters -RunspacePool $pool
-            # processing runspace end
         }
     }
     ### End Runspaces START
